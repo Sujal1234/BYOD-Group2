da541f5de72375f30fc0d92ee68d0e1645c5b6d0
Suuvann Aroraa <suvan24@iitk.ac.in>
2025-07-02 13:30:31 +0530

    updated table.h with function for scan

---
+++ include/table.h
@@ -0,0 +1,21 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "page.h"
+
+#define TABLE_MAX_PAGES 100
+
+typedef struct {
+    Page* pages[TABLE_MAX_PAGES];
+    size_t num_pages;
+    size_t num_rows;
+} Table;
+
+void table_insert_page(Table* table);
+void table_insert_record(Table* table, int64_t id, int32_t age, char* name);
+void scan(Table* table, int64_t id);
+
+#endif //TABLE_H
