2467a22da02c23e2d15a89bf0f2cd6a9d29367ba
shruti00v <shruti132005@gmail.com>
2025-07-02 14:08:32 +0530

    printing the table

---
+++ src/print.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "page.h"
+#include "table.h"
+
+void print_table(Table* table){
+    if(table -> num_pages==0){
+        printf("Table is empty. No data to show\n");
+        return;
+    }
+
+    
+    for(size_t j = 0; j<table->num_pages; j++){
+
+        Page* page = table->pages[j];
+        if(page!=NULL){
+            printf("Page no: %d\n", j);
+
+            for(size_t k = 0; k<page->num_rows; k++){
+                Row* row = &page->rows[k];
+                if(row!=NULL)
+                    printf("S.No: %d, ID: %d, AGE: %d, NAME = %s\n",k,row->id,row->age,row->name);
+                
+            }
+        }
+    }
+}
